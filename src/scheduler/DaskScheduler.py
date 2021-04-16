91d36946fca913415e89115bc24883f6dd26d0c4
Thibault PIRONT <gse.nuks@gmail.com>
2021-04-16 17:17:07 -0400

    cache batch task progressively and log

--- src/scheduler/DaskScheduler.py
+++ src/scheduler/DaskScheduler.py
@@ -76,26 +76,27 @@ class DaskScheduler(LocalScheduler):
         # on every worker, including those setup through dask LocalCluster
         # or MPI, otherwise task scheduling will get stuck.
         computations = client.compute(delayedResults, resources={'job': 1})
-        results = computations.result()
-
-        # Generate a list of successful itemIds.
-        successfulItemIds = [
-            itemId
-            for itemId, taskResult in results.items()
-            if taskResult.didSucceed  # noqa: E712
-        ]
-
-        # Generate a list of unsuccessful itemIds.
-        failedItemIds = [
-            item for item in itemIds if item not in successfulItemIds
-        ]
-
-        # Update cache.
-        for itemId, taskResult in results.items():
+        successfulItemIds = []
+        failedItemIds = []
+        for future, taskResult in dask.distributed.as_completed(computations,
+                                                                loop=client.loop,
+                                                                with_results=True):
+            itemId = list(computations.keys())[list(computations.values()).index(future)]
             taskItemName = f'{taskName}_{str(itemId)}'
             didSucceed = taskResult.didSucceed
             returnCode = taskResult.returnCode
+
+            # Log success/failure.
+            print(f'Task {taskItemName} {"succeeded" if didSucceed else "failed"}.')
+
+            # Update task cache.
             cache.writeTaskCache(taskItemName, didSucceed, returnCode)
+
+            # Generate lists of successful and unsuccessful itemIds.
+            if didSucceed:
+                successfulItemIds.append(itemId)
+            else:
+                failedItemIds.append(itemId)
 
         print(f'Batch {taskName} {str(successfulItemIds)} succeeded.')
         print(f'Batch {taskName} {str(failedItemIds)} failed.')
