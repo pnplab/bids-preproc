b03230f89cda761790b550d61d9f2238b650f893
Thibault PIRONT <gse.nuks@gmail.com>
2021-04-16 00:32:09 -0400

    disable output screen display when log is on

--- src/executor/TaskFactory.py
+++ src/executor/TaskFactory.py
@@ -10,10 +10,10 @@ from ..cli import VMEngine
 def logify(cmd, logPath):
     # @warning
     # relying on `tee` can be/is memory and bandwidth hungry regarding fmriprep
-    # poutput for instance, as log is transfered (if dask) and stored within
+    # output for instance, as log is transfered (if dask) and stored within
     # the result object.
     # return f'''(set -o pipefail && {cmd} 2>&1 | tee "{logPath}")'''
-    return f'''(set -o pipefail && {cmd} 2>&1 | tee "{logPath}")'''
+    return f'''(set -o pipefail && {cmd} 2>&1 > "{logPath}")'''
 
 
 # @warning output cannot be merged into a single.
