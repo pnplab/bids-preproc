b8dc29868d78d9abf2d515dad8b8b4a45bd03df6
Thibault PIRONT <gse.nuks@gmail.com>
2021-04-16 16:40:24 -0400

    prevent fmriprep from processing t1 twice

--- config.py
+++ config.py
@@ -283,6 +283,13 @@ FMRIPREP_SUBJECT = TaskConfig(
 
 # Used to generate fmriprep bids file selection filter for session level
 # granularity.
+#
+# @note we've removed the anat data from the filter, as fmriprep seems to
+# reprocess the anatomical t1 even though we've used anat fast-track.
+# > "flair": {{"datatype": "anat", "suffix": "FLAIR"}},
+# > "t2w": {{"datatype": "anat", "suffix": "T2w"}},
+# > "t1w": {{"datatype": "anat", "suffix": "T1w"}},
+# > "roi": {{"datatype": "anat", "suffix": "roi"}}
 FMRIPREP_SESSION_FILTER = TaskConfig(
     raw_executable='printf',
     singularity_image=None,  # @TODO !!! map to NONE
@@ -301,11 +308,7 @@ FMRIPREP_SESSION_FILTER = TaskConfig(
                 "datatype": "func",
                 "suffix": "sbref",
                 "session": "{sessionId}"
-            }},
-            "flair": {{"datatype": "anat", "suffix": "FLAIR"}},
-            "t2w": {{"datatype": "anat", "suffix": "T2w"}},
-            "t1w": {{"datatype": "anat", "suffix": "T1w"}},
-            "roi": {{"datatype": "anat", "suffix": "roi"}}
+            }}
         }}' > "{bidsFilterFile}"
     ''',
     # Map paths to vm volumes using argument decorators.
